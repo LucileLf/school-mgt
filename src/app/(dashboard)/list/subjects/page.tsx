f1098cef820bf4114cfe6575258e3580c037a446
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2024-10-02 01:46:59 +0200

    fetch subjects

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -4,14 +4,13 @@ import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { role, subjectsData } from "@/lib/data";
+import prisma from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Prisma, Subject, Teacher} from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-type Subject = {
-  id: number;
-  name: string;
-  teachers: string[];
-};
+type SubjectList = Subject &  {teachers: Teacher[]}
 
 const columns = [
   {
@@ -29,31 +28,65 @@ const columns = [
   },
 ];
 
-const SubjectListPage = () => {
-  const renderRow = (item: Subject) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-MySchoolPurpleLight"
-    >
-      <td className="flex items-center gap-4 p-4">{item.name}</td>
-      <td className="hidden md:table-cell">{item.teachers.join(",")}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          <Link href={`/list/students/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-MySchoolSky">
-              <Image src="/edit.png" alt="edit icon" width={16} height={16}/>
-            </button>
-          </Link>
-          {role ==="admin" &&
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-MySchoolPurple">
-                <Image src="/delete.png" alt="trash icon" width={16} height={16}/>
-            </button>
-          }
-        </div>
-      </td>
-    </tr>
-  );
+const renderRow = (item: SubjectList) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-MySchoolPurpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">{item.name}</td>
+    <td className="hidden md:table-cell">{item.teachers.map(teacher=>teacher.name).join(", ")}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        <Link href={`/list/students/${item.id}`}>
+          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-MySchoolSky">
+            <Image src="/edit.png" alt="edit icon" width={16} height={16}/>
+          </button>
+        </Link>
+        {role ==="admin" &&
+          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-MySchoolPurple">
+              <Image src="/delete.png" alt="trash icon" width={16} height={16}/>
+          </button>
+        }
+      </div>
+    </td>
+  </tr>
+);
 
+const SubjectListPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+  
+  // console.log(searchParams);
+  const { page, ...queryParams } = searchParams;
+  const p = page ? parseInt(page) : 1;
+
+  // URL PARAMS CONDITION
+  const query:Prisma.SubjectWhereInput = {};
+
+  if (queryParams) {
+    for(const [key, value] of Object.entries(queryParams)){
+      if(value !== undefined){
+      switch(key){
+        case "search":query.name={contains:value, mode:"insensitive"};
+          break;
+      }}
+    }
+  } 
+
+  const [subjectsData, subjectsCount] = await prisma.$transaction([
+    prisma.subject.findMany({
+      where: query,
+      include: {
+        teachers: true,
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1) // skip items from previous pages
+    }),
+    prisma.subject.count({where:query})
+  ])
+  
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
@@ -75,7 +108,7 @@ const SubjectListPage = () => {
       {/* LIST */}
       <Table columns={columns} renderRow={renderRow} data={subjectsData} />
       {/* PAGINATION */}
-      <Pagination />
+      <Pagination page={p} count={subjectsCount}/>
     </div>
   );
 };
